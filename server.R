4d671598c2d9c6cc6694799bfe30d60edf407d4b
Eric Bailey <ebailey@idem.in.gov>
2014-08-29 16:03:12 -0400

    added geographic data files

--- server.R
+++ server.R
@@ -7,17 +7,8 @@
 
 library(shiny)
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session, clientData) {
 
-  output$distPlot <- renderPlot({
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-  })
 
 })
