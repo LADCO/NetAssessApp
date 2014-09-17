b6e772a2abcb0d71b9b10a0b0dbc59748bc018b9
Nathan Byers <nbyers@idem.in.gov>
2014-09-17 16:21:09 -0400

    downloaded raw data

--- R/newsites/downloadAQS.R
+++ R/newsites/downloadAQS.R
@@ -1,18 +1,9 @@
 library(XML)
 
-
-
-
 # This function downloads a zipped folder from the AQS website, unzips it,
 # and saves the .csv file
 downloadAQS <- function(duration, pollutant.code, year,  
                          destination.directory = "."){
-  
-#   duration <- "daily"
-#   pollutant.code <- "44201"
-#   year <- "2013"
-#   destination.directory <- "C:/R repositories/NetAssessApp/data"
-  
   file.url <- paste0("http://aqsdr1.epa.gov/aqsweb/aqstmp/airdata/", duration,
                      "_", pollutant.code, "_", year, ".zip")
   temp <- tempfile(fileext = ".zip")
@@ -30,18 +21,19 @@ downloadAQS <- function(duration, pollutant.code, year,
 csvToRda <- function(duration, pollutant.code, year, csv.directory, 
                      destination.directory){
   
-#   duration <- "daily"                                   
-#   pollutant.code <- "44201"
-#   year <- "2013"
-#   csv.directory <- "C:/R repositories/NetAssessApp/data/"
-#   destination.directory <- csv.directory
-  
-  csv.file <- paste0(csv.directory, duration, "_", pollutant.code, "_", year,
+  csv.file <- paste0(csv.directory, "/", duration, "_", pollutant.code, "_", year,
                      ".csv")
-  csv.df <- read.csv(csv.file, stringsAsFactors = F)
-  rda.file <- paste0(destination.directory, duration, "_", pollutant.code, "_", 
+  rda.file <- paste0(destination.directory, "/", duration, "_", pollutant.code, "_", 
                      year, ".rda")
-  save(csv.df, file = rda.file)
+  
+  # assign the pasted variable name to the data frame then save
+  assign(paste(duration, pollutant.code, year, sep = "_"), 
+         read.csv(csv.file, stringsAsFactors = F))
+  
+  # evaluate the text
+  eval(parse(text = paste0("save(", duration, "_", pollutant.code, "_", year, ",
+                           file = rda.file)")))
+  
   file.remove(csv.file)
 }
 
@@ -49,10 +41,15 @@ csvToRda <- function(duration, pollutant.code, year, csv.directory,
 # download, unzip, and save as .rda files daily ozone and pm2.5 data for entire
 # country, 2009-2013
 pollutants <- c("44201", "88101", "88502")
-years <- as.character(2009:2011)
+years <- as.character(2009:2013)
 
 downloads.df <- expand.grid(pollutants, years, stringsAsFactors = F)
 
 mapply(downloadAQS, pollutant.code = downloads.df[, 1],  year = downloads.df[, 2], 
        MoreArgs = list(duration = "daily", 
-                       destination.directory = "C:/R repositories/NetAssessApp/data/"))
+                       destination.directory = "C:/R repositories/NetAssessApp/data"))
+
+mapply(csvToRda, pollutant.code = downloads.df[, 1],  year = downloads.df[, 2], 
+       MoreArgs = list(duration = "daily", 
+                       destination.directory = "C:/R repositories/NetAssessApp/data",
+                       csv.directory = "C:/R repositories/NetAssessApp/data"))
