23fd8ecc35d739f5ab47fab3643880eaf4fc2d8e
Nathan Byers <nbyers@idem.in.gov>
2014-09-19 15:54:04 -0400

    created wide tables

--- R/newsites/createTables.R
+++ R/newsites/createTables.R
@@ -1,11 +1,37 @@
 library(XML)
 library(reshape2)
 
+# get the list of ozone .rda files
+files <- list.files("C:/R repositories/NetAssessApp/data")
+files <- paste0("C:/R repositories/NetAssessApp/data/", files[grep("daily_44201", files)])
+
+# load the ozone data frames
+for(i in files) load(i) 
+
+# create long data frame of ozone values
+long.df <- rbind(daily_44201_2009, daily_44201_2010, daily_44201_2011,
+                 daily_44201_2012, daily_44201_2013)
+remove(daily_44201_2009, daily_44201_2010, daily_44201_2011,
+       daily_44201_2012, daily_44201_2013)
+
+# add monitor column
+long.df <- data.frame(Monitor = paste0(sprintf("%02d", long.df$State.Code),
+                                      sprintf("%03d", long.df$County.Code),
+                                      sprintf("%04d", long.df$Site.Num)),
+                     long.df)
+
+# filter out days that don't have 75% completion and drop extra POCs
+long.df <- long.df[long.df$Observation.Percent >= 75, ]
+library(dplyr)
+long.df <- arrange(long.df, Monitor, Date.Local, desc(Observation.Percent), POC)
+duplicates <- duplicated(long.df[, c("Monitor", "Date.Local")])
+long.df <- long.df[!duplicates, ]
+
 # get state and regional numbers
 state.table <- read.csv("https://aqs.epa.gov/aqsweb/codes/data/StateCountyCodes.csv",
-                       skip = 1, stringsAsFactors = F)
+                        skip = 1, stringsAsFactors = F)
 state.regions <- unique(state.table[, c(grep("State.Code", colnames(state.table)),
-                                             grep("Region", colnames(state.table)))])
+                                        grep("Region", colnames(state.table)))])
 
 # create a named list of vectors--name is the regions and the vector contains
 # the states in that region
@@ -14,54 +40,28 @@ list.regions <- lapply(regions, function(x, df) {df[df[, 2] == x, 1]},
                        df = state.regions)
 names(list.regions) <- regions
 
-# get the list of .rda files
-files <- list.files("C:/R repositories/NetAssessApp/data")
-files <- paste0("C:/R repositories/NetAssessApp/data/", files[grep("daily", files)])
 
-makeWideTables <- function(rda.files, regions.list){
+makeWideTable <- function(states, region, pollutant.code, long.data.frame, directory){
+  # region.states <- list.regions["4"]
+  # region <- 4
+  # pollutant.code <- 44201
+  # long.data.frame <- long.df
+  # directory <- "C:/R repositories/NetAssessApp/data"
   
-  # rda.files <- files
-  # regions.list <- list.regions
+  long.sub <- long.data.frame[long.data.frame$State.Code %in% as.integer(states), ]
+  wide.df <- dcast(long.sub, Date.Local ~ Monitor,
+                   value.var = colnames(long.sub)[grep("Max.Value", colnames(long.sub))])
+  wide.df[, "Date.Local"] <- as.Date(wide.df$Date.Local); colnames(wide.df)[1] <- "date"
   
-#   long.data.frames <- sapply(rda.files, 
-#                              function(x){strsplit(x, split = ".", fixed = T)[[1]][1]})
-#   long.data.frames <- sapply(long.data.frames, 
-#                              function(x){strsplit(x, split = "/", fixed = T)[[1]][5]})
+  data.frame.name <- paste("daily", pollutant.code, "region", region, sep = "_")
   
-  for(i in as.character(c(1:10, 25))){
-    for(j in as.character(c(44201, 88101, 88502))){
-      assign(paste("daily", j, "region", i, sep = "_"), 
-             data.frame())
-    }
-  }
+  assign(data.frame.name, wide.df)
   
-  mergeWideDf <- function(long.df.file, regions){
-    # long.df.file <- files[1]
-    # regions <- list.regions
-    load(long.df.file)
-    data.frame.name <- strsplit(long.df.file, split = ".", fixed = T)[[1]][1]
-    data.frame.name <- strsplit(data.frame.name, split = "/", fixed = T)[[1]][5]
-    df <- eval(as.name(data.frame.name))
-    states <- sprintf("%02d", unique(df$State.Code))
-    for(i in states){
-      # i <- states[1]
-      df.sub <- df[df$State.Code == i, ]
-      df.sub <- data.frame(Monitor = paste0(sprintf("%02d", df.sub$State.Code),
-                                            sprintf("%03d", df.sub$County.Code),
-                                            sprintf("%04d", df.sub$Site.Num)),
-                           df.sub)
-      
-      df.sub <- dcast(df.sub,  ~ Monitor)
-      
-      
-      file.region <- names(list.regions)[grep(i, list.regions, fixed = T)]
-      df.region <- strsplit(data.frame.name, "_", fixed = T)[[1]][1:2]
-      df.region <- paste(c(df.region, "region", file.region), collapse = "_")
-      df.region <- eval(as.name(df.region))
-      df.region <- merge(df.region, df.sub, all = T)
-    }
-  }
-  
-  
-  
+  eval(parse(text = paste0("save(", data.frame.name, ", file = '", directory, "/",
+                           data.frame.name, ".rda')")))
 }
+
+
+mapply(makeWideTable, states = list.regions, region = names(list.regions),
+       MoreArgs = list(pollutant.code = 44201, long.data.frame = long.df, 
+                       directory <- "C:/R repositories/NetAssessApp/data"))
