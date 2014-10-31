3143fa93a66b62d6fe06a347e6bd6e84021759a6
Eric Bailey <ebailey@idem.in.gov>
2014-10-31 16:02:50 -0400

    Its all downhill from here

--- server.R
+++ server.R
@@ -79,39 +79,74 @@ shinyServer(function(input, output, session) {
 ##### Area Served Functions
 
   selectedSites <- reactive({
+
     if(input$areaServedCalcButton > 0) {
-      session$sendCustomMessage(type = "showLoading", TRUE)
-      return(sites()[sites()$Key %in% isolate(input$selectedSites), c("Key", "Latitude", "Longitude")])
+      sites <- isolate(sites())
+      return(sites[sites$Key %in% isolate(input$selectedSites), c("Key", "Latitude", "Longitude")])
     }
   })
 
   selectedNeighbors <- reactive({
+
+    ss <- selectedSites()
+    sites <- isolate(sites())
     
-    lats <- list(min(selectedSites()$Latitude), max(selectedSites()$Latitude))
-    lngs <- list(min(selectedSites()$Longitude), max(selectedSites()$Longitude))
+    if(!is.null(ss)) {
+      bounds <- list(list(24.4, -124.8), list(49.4, -66.9))
+      
+      us.lats <- c(24.4, 49.4)
+      us.lngs <- c(-124.8, -66.9)
+      
+      lats <- range(ss$Latitude)
+      lngs <- range(ss$Longitude)
+      lat.rng <- abs(lats[2] - lats[1])
+      lng.rng <- abs(lngs[2] - lngs[1])
+      
+      gtG <- FALSE
+      
+      while(!gtG) {
+        
+        lats.test <- c(max(lats[1] - lat.rng, us.lats[1]), min(lats[2] + lat.rng, us.lats[2]))
+        lngs.test <- c(max(lngs[1] - lng.rng, us.lngs[1]), min(lngs[2] + lng.rng, us.lngs[2]))
+
+        neighbors <- unique(sites[sites$Latitude >= lats.test[1] & sites$Latitude <= lats.test[2] & sites$Longitude >= lngs.test[1] & sites$Longitude <= lngs.test[2], ])
+      
+        new.lats <- range(neighbors$Latitude)
+        new.lngs <- range(neighbors$Longitude)
+        
+        if((new.lats[1] < lats[1] | new.lats[1] <= us.lats[1]) &
+           (new.lats[2] > lats[2] | new.lats[2] >= us.lats[2]) &
+           (new.lngs[1] < lngs[1] | new.lngs[1] <= us.lngs[1]) &
+           (new.lngs[2] > lngs[2] | new.lngs[2] >= us.lngs[2])) {
+          gtG = TRUE
+        }
+        
+        lat.rng <- lat.rng * 2
+        lng.rng <- lng.rng * 2
+        
+      }
+      
+      neighbors <- neighbors[!duplicated(neighbors[, c("Latitude", "Longitude")]), ]
+      v <- deldir(neighbors$Longitude, neighbors$Latitude)
+      x <- v$delsgs
+      x$ind1 <- neighbors$Key[x$ind1]
+      x$ind2 <- neighbors$Key[x$ind2]
+      x <- x[x$ind1 %in% ss$Key | x$ind2 %in% ss$Key, ]
+      x <- unique(c(x$ind1, x$ind2))
     
-    lat.rng <- abs(lats[[2]] - lats[[1]]) * 1.5
-    lng.rng <- abs(lngs[[2]] - lngs[[1]]) * 1.5
+      return(neighbors[neighbors$Key %in% x, ])
     
-    lats <- list(lats[[1]] - lat.rng, lats[[2]] + lat.rng)
-    lngs <- list(lngs[[1]] - lng.rng, lngs[[2]] + lng.rng)
-    
-    neighbors <- unique(sites()[sites()$Latitude >= lats[[1]] & sites()$Latitude <= lats[[2]] & sites()$Longitude >= lngs[[1]] & sites()$Longitude <= lngs[[2]], ])
-    v <- deldir(neighbors$Longitude, neighbors$Latitude)
-    x <- v$delsgs
-    x$ind1 <- neighbors$Key[x$ind1]
-    x$ind2 <- neighbors$Key[x$ind2]
-    x <- x[x$ind1 %in% selectedSites()$Key | x$ind2 %in% selectedSites()$Key, ]
-    x <- unique(c(x$ind1, x$ind2))
-    return(neighbors[neighbors$Key %in% x, ])
+    }
     
   })
 
   polygons <- reactive({
-    if(!is.null(selectedSites())) {
-      if(nrow(selectedSites()) <= 300 & nrow(selectedSites()) >= 2) {
-        v <- voronoi(selectedNeighbors()$Key, selectedNeighbors()$Latitude, selectedNeighbors()$Longitude, usborder)
-        v <- subset(v, id %in% selectedSites()$Key)
+    ss <- isolate(selectedSites())
+    sn <- selectedNeighbors()
+    if(!is.null(ss)) {
+      if(nrow(ss) <= 300 & nrow(ss) >= 2) {
+        v <- voronoi(sn$Key, sn$Latitude, sn$Longitude, usborder)
+        v <- isolate({subset(v, id %in% ss$Key)})
         ov <- over(tracts, v)
         t <- cbind(tracts@data, ov)
         t <- t[!is.na(t$id), ]
@@ -133,19 +168,64 @@ shinyServer(function(input, output, session) {
 
   observe({
     if(!is.null(polygons())) {
-      v <- lapply(polygons()@polygons, function(p) {
-        lapply(p@Polygons, function(pp) {
-          coords <- pp@coords
-          apply(coords, 1, function(r) {
-            list(lat = r[[2]], lng = r[[1]])
-          })
-        })
+      polygons <- polygons()
+      v <- lapply(seq(nrow(polygons)), function(i) {
+        list(id = unlist(strsplit(polygons@polygons[[i]]@ID, " "))[1], 
+             coords = lapply(polygons@polygons[[i]]@Polygons, function(pp) {
+                coords <- pp@coords
+                apply(coords, 1, function(r) {
+                  list(lat = r[[2]], lng = r[[1]])
+                })
+              })
+        )
       })
-      session$sendCustomMessage(type = "hideLoading", TRUE)
       session$sendCustomMessage(type = "showArea", v)
     }
   })
 
+  output$areaServed <- renderText({
+    input$clickedAreaServed
+    polygons <- isolate({polygons()})
+    if(!is.null(polygons)) {
+      data <- polygons@data
+      txt <- paste(data$area[data$id == input$clickedAreaServed], "square km")
+    } else {
+      txt <- ""
+    }
+    return(txt)
+  })
+
+output$totalPopServed <- renderText({
+  input$clickedAreaServed
+  polygons <- isolate({polygons()})
+  if(!is.null(polygons)) {
+    data <- polygons@data
+    txt <- paste(data$total[data$id == input$clickedAreaServed], "Total Population")
+  } else {
+    txt <- ""
+  }
+  return(txt)
 })
 
+  output$agePlot <- renderPlot({
+    input$clickedAreaServed
+    print(1)
+    if(!is.null(input$clickedAreaServed)) {
+      title <- isolate(sites()[sites()$Key %in% input$clickedAreaServed, ])
+      print(title)
+      if(nrow(title) > 0) {
+        
+        title <- paste0("Population served by ",
+                        sprintf("%02i-%03i-%04i", title$State_Code, title$County_Code, title$Site_ID))
+        gg <- agePyramid(polygons()@data, input$clickedAreaServed) + ggtitle(title)
+        print(gg)
+      } else {
+        return(NULL)
+      }  
+      
+    }
+  }, width = 400, height = 450)
+
+
+})
 
